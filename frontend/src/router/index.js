fe2b7ea9bf70560ce8860893aa8d5fb54d1eeb37
dbanasiak <damian.banasiak96@gmail.com>
2018-10-16 22:47:51 +0200

    SETUP: New basic setup front + backend basics

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Register from '@/components/Register'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
