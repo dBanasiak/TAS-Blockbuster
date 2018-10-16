9f3efd44aa0b6f910ddc2ce225f4073729189038
dbanasiak <damian.banasiak96@gmail.com>
2018-10-16 22:50:24 +0200

    SETUP: modified

--- backend/app/models/user.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = mongoose.Schema({
-    id: String,
-    username: String,
-    password: String
-});
-
-module.exports = mongoose.model('User', UserSchema);
