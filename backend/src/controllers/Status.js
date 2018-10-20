530540557cf63f13c3956ccd40c8cb3a4d456a24
dbanasiak <damian.banasiak96@gmail.com>
2018-10-20 14:19:46 +0200

    SETUP: add Joi to validate email and password, add controllers, add policies

---
+++ backend/src/controllers/Status.js
@@ -0,0 +1,7 @@
+module.exports = {
+  status (req, res) {
+    res.send({
+      message: 'Hello user'
+    })
+  }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,10 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+const Status = require('./controllers/Status')
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+
 module.exports = (app) => {
     /* TEST: message Hello user on localhost:8080/status */
-    app.get('/status', (req, res) => {
-        res.send({
-          message: 'Hello user'
-        })
-      })
-      
-      /* API: Register message */
-      app.post('/register', (req, res) => {
-        res.send({
-          message: `Hello ${req.body.email} Your user was registered!`
-        })
-      })
+    app.get('/status', Status.status)
+    /* API: Register message */
+    app.post('/register', AuthenticationControllerPolicy.register ,AuthenticationController.register)
 }
