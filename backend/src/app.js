8a129a167778b0815eb2d2933feb9b70592fbd1f
dbanasiak <damian.banasiak96@gmail.com>
2018-11-02 11:46:20 +0100

    hotfix merge case

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 require('./routes')(app)
 
-sequelize.sync().then(() => {
+sequelize.sync({force:true}).then(() => {
   app.listen(config.port)
   console.log(`Server started on port ${config.port}`)
 })
